98c907aaa1ae351393c0d7968bfe98253a6f0370
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-21 21:17:03 +0200

    onload, onerror, onprogress

--- ajax.js
+++ ajax.js
@@ -4,4 +4,12 @@ button.addEventListener('click', loadText);
 function loadText() {
     var xhr = new XMLHttpRequest();
     console.log(xhr);
+    xhr.open('GET', '1.txt', true);
+
+    xhr.onload = function () {
+        if (this.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+    xhr.send();
 }
